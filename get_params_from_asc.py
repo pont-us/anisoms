37a2ee76a3ad1e71bea6a26d452f1aa1a75aee47
Pont Lurcock <pont@talvi.net>
2016-06-28 13:15:38 +1200

    Make get_params_from_asc read/display more parameters
    get_params_from_asc can now additionally read and display:
    Pj, T, tensor components, and directional parameters in the
    geographic co-ordinate system.

--- get_params_from_asc.py
+++ get_params_from_asc.py
@@ -7,12 +7,17 @@ import re
 class SampleData(object):
     pass
 
+class Directions(object):
+    pass
+
 def process_lines(lines):
     
-    specimen_line_found = False
     current_sample = None
+    current_dirs = None
     samples = []
 
+    factors_line = -1
+    linenum = 0
     for line in lines:
     
         parts = line.split()
@@ -27,28 +32,65 @@ def process_lines(lines):
             parts[2] != "L1"):
             current_sample.mag_sus = parts[2]
 
-        if (specimen_line_found):
-            specimen_line_found = False
-            current_sample.i1 = parts[2]
-            current_sample.i2 = parts[3]
-            current_sample.i3 = parts[4]
+        if (current_dirs): # previous line was "Specimen"/"Geograph"
+            current_dirs.i1 = parts[2]
+            current_dirs.i2 = parts[3]
+            current_dirs.i3 = parts[4]
+            current_dirs.k12 = parts[5]
+            current_dirs.k23 = parts[6]
+            current_dirs.k13 = parts[7]
+            current_dirs = None
 
-        if (len(parts) > 0 and parts[0] == "Specimen"):
+        if (len(parts) > 0 and
+            (parts[0] == "Specimen" or parts[0] == "Geograph")):
+            current_dirs = Directions()
+            if parts[0] == "Specimen":
+                current_sample.dirs_specimen = current_dirs
+            elif parts[0] == "Geograph":
+                current_sample.dirs_geograph = current_dirs
+            else:
+                assert(False)
             specimen_line_found = True
-            current_sample.d1 = parts[2]
-            current_sample.d2 = parts[3]
-            current_sample.d3 = parts[4]
+            current_dirs.d1 = parts[2]
+            current_dirs.d2 = parts[3]
+            current_dirs.d3 = parts[4]
+            current_dirs.k11 = parts[5]
+            current_dirs.k22 = parts[6]
+            current_dirs.k33 = parts[7]
 
+        if "Anisotropy factors" in line:
+            factors_line = linenum + 4
+
+        if (linenum == factors_line):
+            current_sample.Pj = parts[3]
+            current_sample.T = parts[4]
+
+        linenum += 1
+        
     return samples
 
-def print_data(samples, parameter):
+def print_data(samples, parameter, system):
     for sample in samples:
+        if system == "specimen":
+            dirs = sample.dirs_specimen
+        elif system == "geograph":
+            dirs = sample.dirs_geograph
+        else:
+            assert(False)
         if parameter == "magsus":
             print(sample.name, sample.mag_sus)
         elif parameter == "incdec":
             print(sample.name,
-                  sample.i1, sample.d1, sample.i2,
-                  sample.d2, sample.i3, sample.d3)
+                  dirs.i1, dirs.d1, dirs.i2,
+                  dirs.d2, dirs.i3, dirs.d3)
+        elif parameter == "tensor":
+            print(sample.name,
+                  dirs.k11, dirs.k22, dirs.k33,
+                  dirs.k12, dirs.k23, dirs.k13)
+        elif parameter == "pj":
+            print(sample.name, sample.Pj)
+        elif parameter == "t":
+            print(sample.name, sample.T)
 
 def main():
     parser = argparse.ArgumentParser(description =
@@ -58,13 +100,19 @@ def main():
                         help = "an ASC file to read")
     parser.add_argument('--param', "-p", metavar = "parameter-name",
                         type = str, default="magsus",
-                        help = "Parameter to extract (magsus or incdec)")
+                        choices = ["magsus", "incdec", "tensor", "pj", "t"],
+                        help = "Parameter to extract "
+                        "(magsus, incdec, pj, t, or tensor)")
+    parser.add_argument('--system', "-s", metavar = "coordinate-system",
+                        type = str, default="specimen",
+                        choices = ["specimen", "geograph"],
+                        help = "Co-ordinate system (specimen or geograph)")
     args = parser.parse_args()
     for filename in args.ascfiles:
         with open(filename) as fh:
             lines = fh.readlines()
             samples = process_lines(lines)
-            print_data(samples, args.param)
+            print_data(samples, args.param, args.system)
     # TODO if no files use stdin
 
 if __name__=="__main__":
