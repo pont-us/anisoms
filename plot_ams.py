5996e80466d4c076eac72d13ae92393b7c76163a
Pont Lurcock <pont@talvi.net>
2016-10-27 12:38:34 +1300

    Tidy up plot_ams and tensor_to_dir
    plot_ams has a proper command-line interface now. tensor_to_dir
    updated to work with new ams_lib PrincipalDirs and given a docstring.

--- plot_ams.py
+++ plot_ams.py
@@ -1,33 +1,54 @@
 #!/usr/bin/python
 
-from pyx import *
+from pyx import canvas, path
 from ams_lib import PrincipalDirs, Direction
-from math import sqrt, fabs, cos, sin, radians
-import fileinput
+import argparse
 
-canvas = canvas.canvas()
-canvas.stroke(path.circle(0, 0, 10))
+def main():
+    parser = argparse.ArgumentParser(description="Plot AMS directions from an ASC file.")
 
-got_one = False
-for line in fileinput.input():
+    parser.add_argument("input", type=str, nargs=1,
+                        help="ASC file containing data to plot")
+    parser.add_argument("output", type=str, nargs=1,
+                        help="filename for PDF output")
+    parser.add_argument("-c", "--coordinates",
+                        type=str,
+                        choices=["s", "g"], default="g", 
+                        help="co-ordinate system")
+    parser.add_argument("-v", "--verbose",
+                        action="store_true")
+    args = parser.parse_args()
 
-    parts = line.split()
-    if (got_one):
-        got_one = False
-        i1 = float(parts[2])
-        i2 = float(parts[3])
-        i3 = float(parts[4])
-        print d1, i1
-        dirs = PrincipalDirs(
-            Direction.from_polar_degrees(d1, i1),
-            Direction.from_polar_degrees(d2, i2),
-            Direction.from_polar_degrees(d3, i3))
-        dirs.plot(canvas)
-    if (len(parts) > 0 and parts[0] == "Specimen"):
-#    if (len(parts) > 0 and parts[0] == "Geograph"):
-        got_one = True
-        d1 = float(parts[2])
-        d2 = float(parts[3])
-        d3 = float(parts[4])
+    mycanvas = canvas.canvas()
+    mycanvas.stroke(path.circle(0, 0, 10))
+    
+    section_header = {"s" : "Specimen",
+                      "g" : "Geograph"}[args.coordinates]
+    
+    got_one = False
+    with open(args.input[0], "r") as fh:
+        for line in fh.readlines():
+        
+            parts = line.split()
+            if (got_one):
+                got_one = False
+                i1 = float(parts[2])
+                i2 = float(parts[3])
+                i3 = float(parts[4])
+                if args.verbose:
+                    print(d1, i1)
+                dirs = PrincipalDirs(
+                    Direction.from_polar_degrees(d1, i1),
+                    Direction.from_polar_degrees(d2, i2),
+                    Direction.from_polar_degrees(d3, i3))
+                dirs.plot(mycanvas)
+            if (len(parts) > 0 and parts[0] == section_header):
+                got_one = True
+                d1 = float(parts[2])
+                d2 = float(parts[3])
+                d3 = float(parts[4])
+        
+    mycanvas.writePDFfile(args.output[0])
 
-canvas.writePDFfile("plot")
+if __name__ == "__main__":
+    main()
--- tensor_to_dir.py
+++ tensor_to_dir.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python
 
+""" Convert AMS tensors to principal directions
+
+Input: tensors, one to a line, six elements separated by whitespace
+Output: corresponding principal direction for each tensor
+
+Example usage:
+
+./get-tensor-from-asc.py test_data/D_200.ASC | cut -d' ' -f2-7 | ./tensor_to_dir.py
+
+"""
+
 import fileinput
-from ams_lib import *
+from ams_lib import PrincipalDirs
 
 for line in fileinput.input():
     ks = map(float, line.split())
-    ds = PrincipalDirs(ks)
+    ds = PrincipalDirs.from_tensor(ks)
     print ds.p1.to_decinc()
